b0461aae42d6245f57b6f7fd9867426c5e56624f
Gonzal <ibai_gonzalez@euneic.com>
2026-04-29 20:28:26 +0200

    clase

--- AR 4 Ibai Gonzalez/Assets/Ibai/Scripts/Generación.cs
+++ AR 4 Ibai Gonzalez/Assets/Ibai/Scripts/Generación.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 public class Generación : MonoBehaviour
 {
     [SerializeField] List<GameObject> flechas = new List<GameObject>();
+
+    int id;
+    [SerializeField] AudioSource audio;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +16,7 @@ public class Generación : MonoBehaviour
             flechas[i].SetActive(false);
             flechas [i].transform.parent = transform;
         }
+        id = 1;
     }
 
     // Update is called once per frame
@@ -23,10 +27,17 @@ public class Generación : MonoBehaviour
 
     public void Generate()
     {
-        for (int i = 0; i < flechas.Count; i++)
+        flechas[0].SetActive(true);
+    }
+
+    void OnTriggerEnter(GameObject obj)
+    {
+        if(obj.CompareTag("Flecha"))
         {
-            flechas[i].SetActive(true);
-            flechas[i].transform.parent = null;
+            obj.SetActive(false);
+            flechas[id].SetActive(true);
+            id++;
+            audio.Play();
         }
     }
 }
